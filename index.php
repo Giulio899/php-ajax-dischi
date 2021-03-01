81f2dcfe6f839205d6e69221d7feb701676b2153
Giulio <giulio.tavoni@gmail.com>
2021-03-01 16:30:11 +0100

    Definizione html + sass (php da implementare)

---
+++ index.php
@@ -0,0 +1,132 @@
+<?php
+
+  include __DIR__ . '/php-partials/database.php' 
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- CSS -->
+  <link rel="stylesheet" href="css/style.css">
+  <!-- CDN fontawesome -->
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css" integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w==" crossorigin="anonymous" />
+  <!-- Font Google -->
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap" rel="stylesheet">
+  <title>Dischi musicali</title>
+</head>
+<body>
+
+  <header>
+    <nav class="left">
+      <ul>
+        <li><a href="#">Social</a></li>
+        <li><a href="#">Negozi</a></li>
+        <li><a href="#">Eventi</a></li>
+        <li><a href="#">Volantino</a></li>
+        <li><a href="#">Libri Feltrinelli</a></li>
+        <li><a href="#">Laeffe</a></li>
+        <li><a href="#">Carta più</a></li>
+      </ul>
+    </nav>
+    <nav class="right">
+      <ul>
+        <li><a href="#">App</a></li>
+        <li><a href="#">Login</a></li>
+        <li><a href="#">Registrati</a></li>
+        <li><a href="#"><i class="fas fa-shopping-cart"></i></a></li>
+      </ul>
+    </nav>
+  </header>
+
+  <main>
+    <div class="container">
+      <div class="section-container">
+        <h2>Novità per te</h2>
+
+        <div class="album-container">
+          <!-- item 1 -->
+          <div class="item">
+            <img src="img/1.jpg" alt="">
+            <h3>California</h3>
+            <h4>B3N</h4>
+            <small>12,99</small>
+          </div>
+          <!-- item 2 -->
+          <div class="item">
+            <img src="img/1.jpg" alt="">
+            <h3>California</h3>
+            <h4>B3N</h4>
+            <small>12,99</small>
+          </div>
+          <!-- item 3 -->
+          <div class="item">
+            <img src="img/1.jpg" alt="">
+            <h3>California</h3>
+            <h4>B3N</h4>
+            <small>12,99</small>
+          </div>
+          <!-- item 4 -->
+          <div class="item">
+            <img src="img/1.jpg" alt="">
+            <h3>California</h3>
+            <h4>B3N</h4>
+            <small>12,99</small>
+          </div>
+          <!-- item 5 -->
+          <div class="item">
+            <img src="img/1.jpg" alt="">
+            <h3>California</h3>
+            <h4>B3N</h4>
+            <small>12,99</small>
+          </div>
+          <!-- item 6 -->
+          <div class="item">
+            <img src="img/1.jpg" alt="">
+            <h3>California</h3>
+            <h4>B3N</h4>
+            <small>12,99</small>
+          </div>
+          <!-- item 7 -->
+          <div class="item">
+            <img src="img/1.jpg" alt="">
+            <h3>California</h3>
+            <h4>B3N</h4>
+            <small>12,99</small>
+          </div>
+          <!-- item 8 -->
+          <div class="item">
+            <img src="img/1.jpg" alt="">
+            <h3>California</h3>
+            <h4>B3N</h4>
+            <small>12,99</small>
+          </div>
+          <!-- item 9 -->
+          <div class="item">
+            <img src="img/1.jpg" alt="">
+            <h3>California</h3>
+            <h4>B3N</h4>
+            <small>12,99</small>
+          </div>
+          <!-- item 10 -->
+          <div class="item">
+            <img src="img/1.jpg" alt="">
+            <h3>California</h3>
+            <h4>B3N</h4>
+            <small>12,99</small>
+          </div>
+
+        </div>
+
+      </div>
+    </div>
+  </main>
+
+  <footer></footer>
+
+</body>
+</html>
